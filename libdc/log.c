21c397da7cc59ae8820260ae429536309cabd879
sonto <sonto.lau@gmail.com>
2016-02-17 15:20:50 +0800

    Added timezone in log.c

--- libdc/log.c
+++ libdc/log.c
@@ -1,6 +1,20 @@
 #include "libdc.h"
 #include "log.h"
 
+static char *timezone_now (char *buf, int szbuf) {
+    struct tm tm;
+    struct timeval time;
+    char tmbuf[50] = {0};
+    //time_t t = time (NULL);
+
+    gettimeofday (&time, NULL);
+    localtime_r (&time.tv_sec, &tm);
+    strftime (tmbuf, sizeof (tmbuf), "%F %T",  &tm);
+
+    snprintf (buf, szbuf, "%s.%lu", tmbuf, time.tv_usec);
+    return buf;
+}
+
 void DC_log (const char *tag, const char *fmt, ...)
 {
     FILE *fp = stdout;
@@ -37,8 +51,11 @@ void DC_log (const char *tag, const char *fmt, ...)
         rewind (logfp);
     }
 */
+    char timebuf[255] = {0};
+
+    timezone_now (timebuf, sizeof (timebuf)-1);
     va_start(arg_ptr, fmt);
-    fprintf (fp, "%s\t", tag);
+    fprintf (fp, "%s %s\t", timebuf, tag);
     vfprintf (fp, fmt, arg_ptr);
     fprintf (fp, "\n");
     fflush (fp);
